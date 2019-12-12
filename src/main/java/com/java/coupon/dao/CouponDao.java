d22fef64cd53958c6e1335f658eb53d167b87872
jiwonJ <labelle410@gmail.com>
2019-12-12 10:05:00 +0900

    191212 coupon 패키지 생성

---
+++ src/main/java/com/java/coupon/dao/CouponDao.java
@@ -0,0 +1,10 @@
+package com.java.coupon.dao;
+
+/**
+ * @작성자 : 전지원
+ * @작업일 : 2019. 12. 12.
+ * @작업 내용 : CouponDao 생성
+ */
+public interface CouponDao {
+	
+}
---
+++ src/main/java/com/java/coupon/dto/CouponDto.java
@@ -0,0 +1,10 @@
+package com.java.coupon.dto;
+
+/**
+ * @작성자 : 전지원
+ * @작업일 : 2019. 12. 12.
+ * @작업 내용 : CouponDto 생성
+ */
+public interface CouponDto {
+
+}
